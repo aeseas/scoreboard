89c144a75710579ab645bff05c36be07ee3eeb69
Aisha El Salawi <aisha.elsalawi@aol.com>
2019-04-16 09:52:23 +0200

    update

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Counter = (props) => {
-
-  let index = props.index;
+const Counter = ({ index, score, changeScore }) => {
+  // same as in Header.js 
+  // let index = props.index;
 
   return (
     <div className="counter">
-      <button className="counter-action decrement" onClick={() => props.changeScore(index, -1)}> - </button>
-      <span className="counter-score">{ props.score }</span>
-      <button className="counter-action increment" onClick={() => props.changeScore(index, 1)}> + </button>
+      <button className="counter-action decrement" onClick={() => changeScore(index, -1)}> - </button>
+      <span className="counter-score">{ score }</span>
+      <button className="counter-action increment" onClick={() => changeScore(index, 1)}> + </button>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,14 @@ import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
 
-const Header = (props) => {
+const Header = ({players, title}) => {
+  // OF
+  // const { players, title } = props;
+  // ipv props.players, props.title
   return (
     <header>
-      <Stats players={props.players}/>
-        <h1>{ props.title }</h1>
+      <Stats players={players}/>
+        <h1>{ title }</h1>
         <Stopwatch />
     </header>
     
